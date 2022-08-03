8c5fcc422d1848c815b887c870e8867e55dec5e5
“Craig <97258442+craigbucher@users.noreply.github.com>
2022-08-02 20:57:56 -0500

    initial django

---
+++ assess5_proj/assess5_app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+               
+def send_the_homepage(request):
+    print('home!')
+    # theIndex = open('static/index.html').read()
+    # return HttpResponse(theIndex)
